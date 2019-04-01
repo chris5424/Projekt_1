ffd96be1e77c25a50a3ba2db3d68c7bdc8ec7537
Krzysztof Sitarz <chris5424@protonmail.com>
2019-04-02 01:22:26 +0200

    Działa wszystko, testy przeprowadzone. Wstępne opisywanie.

--- Projekt1/Projekt1/main.cpp
+++ Projekt1/Projekt1/main.cpp
@@ -16,108 +16,6 @@
 
 int main()
 {
-	////std::cout << "Podaj wielkosc tablicy do posortowania: ";
-	//
-	//int wielkosc=1000000;
-	////std::cin >> wielkosc;
-	//
-	//
-	//
-	//int** tablica = new int *[100];
-	//for (int row = 0; row < 99; row++)  //utworzenie 100 tablic
-	//{
-	//	tablica[row] = new int[wielkosc];
-	//}
-	//
-	//
-	//for (int row = 0; row < 99; row++)
-	//{
-	//	for (int i = 0; i < wielkosc; i++)
-	//	{
-	//		tablica[row][i] = rand();  //wype³nienie tablic liczbami losowymi
-	//	}
-	//}
-	//
-	//
-	////for (int row = 0; row < 99; row++)
-	////{
-	////	for (int i = 0; i < wielkosc; i++)
-	////	{
-	////		std::cout << tablica[row][i] << " ";  //wyœwietlenie tablic nieposortowanych
-	////	}
-	////	std::cout << "\n";
-	////}
-	////std::cout << "\n";
-	//
-	//for (int i = 0; i < 99; i++)
-	//{
-	//	quicksort(tablica[i], 0, wielkosc - 1, 0.5);  //wstêpne sortowanie
-	//	//inv_quicksort(tablica[i], 0, wielkosc - 1);    //sortowanie odwrotne
-	//}
-	//std::cout << "sortujemy";
-	////auto start = std::chrono::system_clock::now(); //start czasu
-	//for (int i = 0; i < 99; i++)
-	//{
-	//	auto start = std::chrono::system_clock::now();
-	//	//int *tab_pom = new int[wielkosc];
-	//	//bubble(tablica[i], wielkosc);
-	//	//insert(tablica[i], wielkosc);
-	//	//quicksort(tablica[i], 0, wielkosc - 1, 1);  //czwarty parametr to stopieñ posortowania w % (0-1)
-	//	//merge_sort(tablica[i], tab_pom, 0, wielkosc-1);
-	//	//heapsort(tablica[i], wielkosc);
-	//	intro_sort(tablica[i],wielkosc);
-	//	//IntroSort(tablica[i], wielkosc);
-	//	auto end = std::chrono::system_clock::now();
-	//	std::chrono::duration<double> elapsed_seconds = end - start;
-	//	if (czy_posortowane1x1(tablica[i], wielkosc) == 1)
-	//	{
-	//		std::cout << "Posortowane\nCzas sortowania: " << elapsed_seconds.count() << "s\n";
-	//		std::cout << "\n";
-	//	}
-	//	else
-	//	{
-	//		std::cout << "Blad sortowania\n";
-	//	}
-	//}
-	////auto end = std::chrono::system_clock::now(); //koniec czasu
-	//
-	////std::chrono::duration<double> elapsed_seconds = end - start;  //obliczenie zmiany czasu
-	//
-	//if (czy_posortowane(tablica, wielkosc)==1)
-	//{
-	//	std::cout << "Posortowane\nCzas sortowania: " << elapsed_seconds.count() << "s\n";
-	//	std::cout << "\n";
-	//}
-	//else
-	//{
-	//	std::cout << "Blad sortowania\n";
-	//}
-	//
-	//
-	////for (int row = 0; row < 99; row++)
-	////{
-	////	for (int i = 0; i < wielkosc; i++)
-	////	{
-	////		std::cout << tablica[row][i] << " ";  //wyœwietlenie tablic posortowanych
-	////	}
-	////	std::cout << "\n";
-	////}
-	////std::cout << "\n";
-	//
-	//
-	//
-	////for (int i = 0; i < 99; i++)  
-	////{
-	////	delete[] tablica[i];   //zwolnienie pamiêci
-	////}
-	////delete[] tablica;
-	////tablica = NULL;
-	//
-	//
-	//
-	//system("pause");
-	//return 1;
-
 int wiersz = 100;
 int rozmiaryTablic[5] = { 10000,50000, 100000, 500000, 1000000 };
 double wstepnePosortowanie[8] = { -1, 1, 0.25, 0.5 , 0.75, 0.95, 0.99, 0.997 };
@@ -125,19 +23,18 @@ std::fstream plik;
 int **tablica = new int *[wiersz];
 for (int rozmiar = 0; rozmiar < 5; rozmiar++)
 {
-	std::cout << "Kolejny rozmiar ********* \n";
+	std::cout << "Rozmiary: "<< rozmiar+1<<"/5 "<<rozmiaryTablic[rozmiar]<<std::endl;
 	for (int posortowanie = 0; posortowanie < 8; ++posortowanie)
 	{
-		std::cout << "Kolejny stopien posortowania\n";
+		std::cout << "Stopien wstepnego posortowania: "<<posortowanie+1<<"/8"<<std::endl;
+
 		// utworzenie i wype³nienie 
 		for (int i = 0; i < wiersz; ++i)
 		{
-			std::cout << "Kolejna tablica\n";
 			tablica[i] = new int[rozmiaryTablic[rozmiar]];
 			for (int j = 0; j < rozmiaryTablic[rozmiar] - 1; ++j)
 				tablica[i][j] = rand();
 		}
-		//std::cout << posortowanie << " \n";
 
 		//warunek wstêpnego sortowania odwrotnego
 		if (wstepnePosortowanie[posortowanie] == -1.0)
@@ -165,10 +62,10 @@ for (int rozmiar = 0; rozmiar < 5; rozmiar++)
 
 			//Tu wpisujemy sortowanie
 			//insert(tablica[i], rozmiaryTablic[rozmiar]);
-			//quicksort(tablica[i], 0, rozmiaryTablic[rozmiar] - 1, 1); //czwarty parametr to stopieñ posortowania w % (0-1)
-			//mergesort(tablica[i],tablica_pomocnicza, 0, rozmiaryTablic[rozmiar] - 1);
+			quicksort(tablica[i], 0, rozmiaryTablic[rozmiar] - 1, 1); //czwarty parametr to stopieñ posortowania w % (0-1)
+			//merge_sort(tablica[i],tablica_pomocnicza, 0, rozmiaryTablic[rozmiar] - 1);
 			//heapsort(tablica[i], rozmiaryTablic[rozmiar]);
-			intro_sort(tablica[i], rozmiaryTablic[rozmiar]);
+			//intro_sort(tablica[i], rozmiaryTablic[rozmiar]);
 			auto end = std::chrono::system_clock::now();
 
 			//sprawdzenie poprawnoœci posortowania
@@ -179,8 +76,8 @@ for (int rozmiar = 0; rozmiar < 5; rozmiar++)
 				if (plik.good() == true)
 				{
 
-					plik << i << " " << rozmiaryTablic[rozmiar] << " " << wstepnePosortowanie[posortowanie] << " ";
-					plik << " " << elapsed_seconds.count() << std::endl;
+					plik << i << ";" << rozmiaryTablic[rozmiar] << ";" << wstepnePosortowanie[posortowanie] << ";";
+					plik << elapsed_seconds.count() << std::endl;
 					plik.close();
 				}
 			}
@@ -201,11 +98,10 @@ for (int rozmiar = 0; rozmiar < 5; rozmiar++)
 			delete[] tablica[q]; //zwolnienie pamiêci
 		}
 	}
-	
 }
+std::cout << "Zakonczono pomiary :)"<<std::endl;
 delete[] tablica;
 tablica = NULL;
 system("pause");
 return 0;
-
 }
